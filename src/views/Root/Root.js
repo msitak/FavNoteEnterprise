1d751e8152e154b13acbba7195a8115f24327b4b
x-kom Michał Sitak <michal.sitak@x-kom.pl>
2019-12-23 09:49:12 +0100

    test lint-staged and husky

---
+++ src/views/Root/Root.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Root = () => (
+  <h1>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias at ea fugiat illo. Atque,
+    natus, quas! Alias asperiores atque consectetur distinctio dolor eligendi excepturi explicabo
+    impedit mollitia pariatur perspiciatis quia quisquam saepe sunt suscipit, tenetur veniam!
+    Asperiores autem blanditiis commodi error eum eveniet impedit ipsa, libero soluta. Aliquid aut
+    commodi dolor doloribus voluptas? Beatae corporis cum cupiditate, deserunt doloremque dolorum,
+    eaque et id illo iure laudantium magnam magni mollitia nobis nulla odio officia possimus quam
+    quos ratione saepe sit unde voluptatum. Doloremque doloribus facere obcaecati odit unde.
+    Consequuntur culpa dolorem ducimus eaque fuga illum quam reiciendis sit veniam voluptatem?
+  </h1>
+);
+
+export default Root;
